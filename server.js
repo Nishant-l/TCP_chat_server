88efa3d425a5ba978e02d16439319daa43020ddc
Nishant <nishant3151998@gmail.com>
2021-05-07 01:20:23 +0530

    🎨 : adding new feature

--- server.js
+++ server.js
@@ -7,24 +7,40 @@ server.listen(port,function onListen(){
     console.log(`now listening on port ${port}`);
 })
 function onConnection(conn){
-    const message=`Welcome to TCP chat room ${count} other people are connected ...\n \\ Please write your Name and press return \n`;
+    const message=`Welcome to TCP chat room ${count} other people are connected...\\n
+                   Please write your Name and press return\n`;
     conn.setEncoding('utf8');
     conn.write(message);
-    let name;
+    let name=undefined;
     count+=1;
     conn.on('data',function onData(data){
         if(!name){
-            if(users[data]){
-                conn.write(`Name:${data} alredy exist, Choose anathear name`);
+            if(users[name]){
+                conn.write(`Name:${data} alredy exist, Choose anathear name\n`);
 
             }
+            else{
+                name=data;
+                users[name]=conn;
+                Object.keys(users)
+                      .forEach(user=>{
+                          users[user].write(`${name}has joined the chat room\n`);
+                      });
+            }
         }else{
-            conn.write(`${name}:${data} alredy exist`);
+            Object.keys(users)
+                  .forEach(user => {
+                    users[user].write(`${name}:${data}`);
+                  })
+            
         }
     });
     conn.on('close',function onClose(){
         count-=1;
         delete users[name];
-        console.log(`${name} has left!\n`);
+        Object.keys(users)
+              .forEach(user => {
+                  users[user].write(`${name} Has left the chat\n`);
+              });
     });
 }
